d68b20398283e6e5834c8e540fce2065810b4510
Mosaif Khan <mosaif786.mk@gmail.com>
2025-10-27 06:49:00 +0300

    Authentication

---
+++ app/routes/Home.tsx
@@ -0,0 +1,44 @@
+import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resume} from "react-dom/server";
+import ResumeCard from "~/components/ResumeCard";
+import {resumes} from "../../constants";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+
+export default function Home() {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+      <section className= "Main-section">
+        <div className="page-heading py-16">
+            <h1>Track Your Application & Resume Readings</h1>
+            <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+    </section>
+      {resume.length > 0 && (
+          <div className="resume-section">
+              {resumes.map((resume) => (
+                  <ResumeCard key={resume.id} resume={resume} />
+              ))}
+          </div>
+)}
+  </main>
+}
